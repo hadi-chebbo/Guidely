eda6de454779d5b4e60bd36af35c1243657d3bb7
Jad Obeid <o.jad.manar@gmail.com>
2026-04-25 15:26:32 +0300

    TI-25: Scaffold majors page with reusable component

---
+++ backend/resources/views/components/major-card.blade.php
@@ -0,0 +1,10 @@
+<div class="card">
+    <h3>{{ $major['name'] }}</h3>
+
+    <span class="badge">{{ $major['category'] }}</span>
+    <span class="badge">{{ $major['demand'] }} Demand</span>
+
+    <p>{{ $major['description'] }}</p>
+
+    <a href="#">View details</a>
+</div>
