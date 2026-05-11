c2f6e8a1d4a8fc803b3ef20fdcbbe42e19375e67
Rony abou ezzi <ronyabouezzi03@gmail.com>
2026-05-11 21:42:03 +0300

    rename student favorite route file

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function (): void {
     require __DIR__.'/api/auth.php';
-    require __DIR__.'/api/student/favorite.php';
+    require __DIR__.'/api/student/major.php';
     require __DIR__.'/api/admin/major.php';
     require __DIR__.'/api/admin/university.php';
     require __DIR__.'/api/admin/faq.php';
