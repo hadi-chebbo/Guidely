e92ebc54fee72683b3b888b2b47a258a0d728871
fatima janoun <fatimajannoun09@gmail.com>
2026-05-20 23:23:30 +0300

    bug fix

--- backend/routes/api/student/mentor.php
+++ backend/routes/api/student/mentor.php
@@ -8,7 +8,7 @@ Route::prefix('mentors')
     ->group(function (): void {
         Route::get('/{user:username}', 'show');
         Route::get('/' , 'index');
-        Route::post('/apply','apply');
+        Route::post('/apply','apply')->middleware('auth:sanctum');;
     });
 
     
